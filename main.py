eca6a4c827996562d7eb559df3c72df07a34cf99
apsamuel <aaron.psamuel@gmail.com>
2021-06-06 04:50:46 -0400

    import bokeh themes module

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 import bokeh
 from operator import ge
 from bokeh.plotting import figure, curdoc
-from bokeh.themes import built_in_themes
+from bokeh.themes import Theme, built_in_themes
 #import lib.datasources as datasources
 #import lib.genericCountry as genericCountry
 # sys.path.append("./lib")
@@ -22,12 +22,18 @@ bokeh_version=bokeh.__version__
 # df_full = datasources.fixtures(
 #     datasources.fulldb()[0]
 #     )
-
+#originally using 2.2.3 version of bokeh. 
 def main():
     if os.environ.get('DEBUG_APP', 'false') == "true":
         debugpy.listen(('127.0.0.1', 5678))
         debugpy.wait_for_client()
     print("Starting main function...")
+
+
+    x = [1, 2, 3, 4, 5]
+    y = [6, 7, 6, 4, 5]
+    p = figure(title='contrast', name='simpleplot',plot_width=300, plot_height=300)
+    p.line(x, y)
     r = genericCountryX()
     #r.countries()
 
@@ -37,10 +43,13 @@ def main():
     #print(type(layout))
     #print(layout)
     curdoc().template_variables["page_layout"] = layout
-    curdoc().theme = 'dark_minimal'
+    curdoc().theme = Theme(filename='./theme.yaml')
     curdoc().add_root(
         layout
         #r.panel().servable()
+    )
+    curdoc().add_root(
+        p
     )
     print("page rendering complete, please validate results")
 
