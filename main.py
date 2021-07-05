a28639eeac046cf15e4d65aebbc3ec4c55293f88
apsamuel <aaron.psamuel@gmail.com>
2021-07-05 12:12:53 -0400

    dev: update messaging & imports

--- main.py
+++ main.py
@@ -4,30 +4,38 @@
 # from lib import datasources as datasources
 
 import bokeh
+import pandas as pd
 from operator import ge
+import debugpy
+import os
+import sys
+import logging
 from bokeh.plotting import figure, curdoc
 from bokeh.themes import Theme, built_in_themes
-#import lib.datasources as datasources
-#import lib.genericCountry as genericCountry
-# sys.path.append("./lib")
-#from lib.genericCountry import genericCountry 
 from lib.FullDataTable import FullDataTable
 from lib.DataOverview import DataOverview
 from lib.ChoroplethMap import ChoroplethMap
 from lib import datasources
-import debugpy 
-import os, sys
-import logging
-logging.basicConfig(stream=sys.stdout, level=print)
-bokeh_version=bokeh.__version__
-# df_full = datasources.fixtures(
-#     datasources.fulldb()[0]
-#     )
-#originally using 2.2.3 version of bokeh. 
+import lib.loginator 
+log = logging.getLogger(__name__)
+logger = lib.loginator.Loginator(
+    logger=log,
+    lvl='DEBUG'
+).logger
+
+# setup some things..
 
 
 def main():
+    bokeh_version = bokeh.__version__
+    pandas_version = pd.__version__
+    #debugpy.breakpoint()
+    logger.debug("[main] starting main steps")
+    logger.debug(f"[main] Pandas Version: {pandas_version}")
+    logger.debug(f"[main] Bokeh Version: {bokeh_version}")
 
+    # data records
+    logger.debug("[main] beginning full_db data source init")
     full_db = datasources.fixtures(
                 datasources.fulldb()[0]
                 )
@@ -38,6 +46,7 @@ def main():
             )
         )
     )
+    #debugpy.breakpoint()
     geo_db = datasources.geodb(
         datasources.groupdb(
             datasources.fixtures(
@@ -47,33 +56,33 @@ def main():
         datasources.shapesdb(),
         datasources.codedb()
     )
+    #debugpy.breakpoint()
+    logger.debug("[main] data init complete")
 
-    # start debugging session
-    if os.environ.get('DEBUG_APP', 'false') == "true":
-        debugpy.listen(('127.0.0.1', 5678))
-        debugpy.wait_for_client()
-    print("Starting main function...")
+    # optionally start debugging session
+    # if os.environ.get('DEBUG_APP', 'false') == "true":
+    #     logger.debug("debug mode has been enabled, please connect a client to port 5678")
+    #     debugpy.listen(('127.0.0.1', 5678))
+    #     debugpy.wait_for_client()
 
 
-    # x = [1, 2, 3, 4, 5]
-    # y = [6, 7, 6, 4, 5]
-    # p = figure(title='contrast', name='simpleplot',sizing_mode='scale_both')
-    # p.line(x, y)
-    # p.css_classes = [
-    #     'plot'
-    # ]
+    # add main content to DOM
+    logger.debug("[main] generating page layout objects")
+
+    #debugpy.debug_this_thread()
     full_table = FullDataTable(db=full_db)
     overview = DataOverview(db=daywise_db)
     overview_layout = overview.layout(title='overview')
     choro = ChoroplethMap(db=geo_db)
     choro_layout = choro.layout()
     table_layout = full_table.layout()
+    logger.debug("[main] layout object generation complete")
 
+    # current document settings
     curdoc().template_variables["page_layout"] = table_layout
     curdoc().theme = Theme(filename='./theme.yaml')
     curdoc().add_root(
         table_layout
-        #r.panel().servable()
     )
     curdoc().add_root(
         overview_layout
@@ -81,6 +90,16 @@ def main():
     curdoc().add_root(
         choro_layout
     )
-    print("page rendering complete, please validate results")
+
+    # fin... 
+    logger.debug("[main] page has been rendered, please do validate the results in a browser")
+    logger.debug('[main] starting debugging')
+    #debugpy.listen(('127.0.0.1', 5678))  # testing a breakpoint
+    
+    if os.environ.get('DEBUG_APP', False) == 'true':
+        logger.debug("[main] entering breakpoint")
+        debugpy.breakpoint()
+    logger.debug("[main] completed breakpoint")
 
 main()
+
